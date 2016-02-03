fb7a92d979bfbf7bc52a5fbc69a910802bed9308
Chandra Mangipudi <mcprakash2009@gmail.com>
2016-02-03 14:38:39 -0600

    Update shaders
    Update shaders

--- picam_gpu/shaders/hsvfragshader.glsl
+++ picam_gpu/shaders/hsvfragshader.glsl
@@ -1,3 +1,24 @@
+##########################################################################
+#  HSV Frag Shader														 #
+#																		 #
+#  Shader to convert RGB color space to HSV			 				     #
+##########################################################################
+#  Copyright(C)  <December 2015>  <Chandra P. Mangipudi					 #
+#																		 #
+#  This program is free software : you can redistribute it and/or modify #
+#  it under the terms of the GNU General Public License as published by  # 
+#  the Free Software Foundation, either version 3 of the License, or	 #
+#  (at your option) any later version.									 #
+#																		 #
+#  This program is distributed in the hope that it will be useful,		 #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of		 #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the			 #
+#  GNU General Public License for more details.							 #
+#																		 #
+#  You should have received a copy of the GNU General Public License	 #
+#  along with this program.If not, see <http://www.gnu.org/licenses/>.	 #
+##########################################################################
+
 varying vec2 tcoord;
 uniform sampler2D tex;
 
@@ -9,7 +30,7 @@ vec3 rgb2hsv(vec3 c)
 
     float d = q.x - min(q.w, q.y);
     float e = 1.0e-10;
-    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 # d + e)), d / (q.x + e), q.x);
 }
 
 void main(void) 
