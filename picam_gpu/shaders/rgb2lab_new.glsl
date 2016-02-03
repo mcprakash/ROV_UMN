9e67b2d670d5bc96277b882bc2014b9510dad8e6
Chandra Mangipudi <mcprakash2009@gmail.com>
2016-02-03 14:46:00 -0600

    Update Shaders
    Update Shaders

--- picam_gpu/shaders/rgb2lab_new.glsl
+++ picam_gpu/shaders/rgb2lab_new.glsl
@@ -1,22 +1,22 @@
 ##########################################################################
-#  LAB Frag Shader														 #
-#																		 #
-#  Shader to convert RGB color space to LAB			 				     #
+#  LAB Frag Shader														 
+#																		 
+#  Shader to convert RGB color space to LAB			 				     
 ##########################################################################
-#  Copyright(C)  <December 2015>  <Chandra P. Mangipudi					 #
-#																		 #
-#  This program is free software : you can redistribute it and/or modify #
-#  it under the terms of the GNU General Public License as published by  # 
-#  the Free Software Foundation, either version 3 of the License, or	 #
-#  (at your option) any later version.									 #
-#																		 #
-#  This program is distributed in the hope that it will be useful,		 #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of		 #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the			 #
-#  GNU General Public License for more details.							 #
-#																		 #
-#  You should have received a copy of the GNU General Public License	 #
-#  along with this program.If not, see <http://www.gnu.org/licenses/>.	 #
+#  Copyright(C)  <December 2015>  <Chandra P. Mangipudi					 
+#																		 
+#  This program is free software : you can redistribute it and/or modify 
+#  it under the terms of the GNU General Public License as published by  
+#  the Free Software Foundation, either version 3 of the License, or	 
+#  (at your option) any later version.									 
+#																		 
+#  This program is distributed in the hope that it will be useful,		 
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of		 
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the			 
+#  GNU General Public License for more details.							 
+#																		 
+#  You should have received a copy of the GNU General Public License	 
+#  along with this program.If not, see <http://www.gnu.org/licenses/>.	 
 ##########################################################################
 
 vec4 rgb2xyz( vec4 rgb)
